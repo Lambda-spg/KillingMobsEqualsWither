e9347f8060b00715ab953dab1f369fda47f78ad3
LambdaSpg <LambdaSpg@protonmail.com>
2021-01-16 01:14:08 +0100

    Initial commit

---
+++ src/main/java/de/spawnwither/spawnwither/SpawnWither.java
@@ -0,0 +1,17 @@
+package de.spawnwither.spawnwither;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnWither extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
