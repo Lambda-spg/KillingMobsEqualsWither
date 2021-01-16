508bc1aa66852a122527fe2f7aeb915ddf050618
LambdaSpg <LambdaSpg@protonmail.com>
2021-01-16 04:04:12 +0100

    Version 1.0 upload

--- src/main/java/de/spawnwither/spawnwither/listeners/killMobsListener.java
+++ src/main/java/de/spawnwither/spawnwither/listeners/killMobsListener.java
@@ -3,7 +3,6 @@ package de.spawnwither.spawnwither.listeners;
 import de.spawnwither.spawnwither.SpawnWither;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
